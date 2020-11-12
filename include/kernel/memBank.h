ba95ed9427c237f469d7230128e79ab97341f226
SmokeyTheKat <owr5273@gmail.com>
2020-11-12 15:29:46 -0400

    started memBank

--- include/kernel/memBank.h
+++ include/kernel/memBank.h
@@ -3,25 +3,44 @@
 
 #include "../ddcDef.h"
 
+struct membank;
+struct memRepo;
+struct MBDN;
+
+struct memBank
+{
+	void* addr;
+	uint64t size;
+	void* vaultAddr;
+	uint64t vaultLength;
+	struct MBDN* headRepo;
+	bool isLocked;
+};
+
 struct memRepo
 {
 	void* address;
 	uint64t size;
 	int32t uid;
+	bool isFree;
 	bool isLocked;
 	bool isCache;
 	bool isEmpty;
 };
 
-struct memBank
+struct MBDN
 {
-	void* bankAddr;
-	uint64t bankSize;
-	void* bankVaultAddr;
-	void* bankVaultLength;
-	struct memRepo* headRepo;
-	bool isLocked;
+	struct MBDN* next;
+	struct MBDN* prev;
+	struct memRepo repo;
 };
 
+void init_memBank(void);
+void memBank_get_real_addr(struct memBank bank, void* bankAddr);
+struct MBDN* memBank_get_free_repo(struct memBank bank);
+
+struct memRepo make_memRepo(void* addr, uint64t size, int32t uid, bool locked);
+
+struct MBDN make_MBDN(struct MBDN* next, struct MBDN* prev, struct memRepo repo);
 
 #endif
--- kernel/memBank.c
+++ kernel/memBank.c
@@ -1,3 +1,59 @@
 #include "../include/kernel/memBank.h"
 
+#define DEFAULT_REPO_SIZE 4096
 
+static struct memBank mbank;
+
+void* memBank_get_real_addr(struct memBank bank, void* bankAddr)
+{
+	return bank.addr + bankAddr;
+}
+
+struct MBDN* memBank_get_free_repo(struct memBank bank)
+{
+	struct MBDN* rptr = bank.headRepo;
+	while (rptr->next != nullptr && !rptr->next.isFree)
+	{
+		rptr = rptr->next;
+	}
+	if (rptr->next == nullptr)
+	{
+		//rptr->next = make_memRepo(rptr->
+	}
+	else if (rptr->next.isFree == true)
+	{
+
+	}
+}
+
+void init_memBank(void)
+{
+	mbank.addr = nullptr + 2048;
+	mbank.size = 8192;
+	mbank.vauleAddr = nullptr;
+	mbank.vauleSize = 2048;
+	struct memRepo mrepo = make_memRepo(memBank_get_real_addr(mbank, 0x00000000), DEFAULT_REPO_SIZE, -1, false);
+	struct MBDN hrepo = make_MBDN(nullptr, nullptr, mrepo);
+	mbank.headRepo = hrepo;
+	mbank.isLocked = false;
+}
+
+struct memRepo make_memRepo(void* addr, uint64t size, int32t uid, bool locked)
+{
+	struct memRepo output;
+	output.address = addr;
+	output.size = size;
+	output.uid = uid;
+	output.isLocked = locked;
+	output.isFree = true;
+	return output;
+}
+
+struct MBDN make_MBDN(struct MBDN* next, struct MBDN* prev, struct memRepo repo)
+{
+	struct MBDN output;
+	output.next = next;
+	output.prev = prev;
+	output.repo = repo;
+	return output;
+}
