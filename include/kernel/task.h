6bee9363110461d600bd5f3e559213ca2d1735e5
= <owr5273@gmail.com>
2020-11-16 01:15:17 -0400

    started co op tasking

---
+++ include/kernel/task.h
@@ -0,0 +1,17 @@
+#ifndef __ddmos_task_h__
+#define __ddmos_task_h__
+
+#include "./system.h"
+
+struct task
+{
+	regs32t regs;
+	struct task* nextTask;
+};
+
+void init_task(void);
+void task_create(struct task* task, void(*code)(void), uint32t flags, uint32t* memRepo);
+void task_switch(regs32t* newRegs, regs32t* oldRegs);
+void task_yield(void);
+
+#endif
---
+++ kernel/task.c
@@ -0,0 +1,41 @@
+#include "../include/kernel/task.h"
+#include "../include/kernel/kernel.h"
+#include "../include/kernel/tty.h"
+
+static struct task* activeTask;
+static struct task mainTask;
+static struct task otherTask;
+
+static void otherMain(void)
+{
+	ddtty_write_cstring(&g_kernelTerm2, "OTHER MAIN FUNCTION\n");
+	task_yield();
+}
+
+void init_task(void)
+{
+	asm volatile("movl %%cr3, %%eax; movl %%eax, %0;":"=m"(mainTask.regs.cr3)::"%eax");
+	asm volatile("pushfl; movl (%%esp), %%eax; movl %%eax, %0; popfl;":"=m"(mainTask.regs.eflags)::"%eax");
+	task_create(&otherTask, otherMain, mainTask.regs.eflags, (uint32t*)&(mainTask.regs.cr3));
+	mainTask.nextTask = &otherTask;
+	otherTask.nextTask = &mainTask;
+	activeTask = &mainTask;
+}
+
+void task_create(struct task* task, void(*code)(), uint32t flags, uint32t* memRepo)
+{
+	task->regs.eax = 0;
+	task->regs.ebx = 0;
+	task->regs.ecx = 0;
+	task->regs.edx = 0;
+	task->regs.esi = 0;
+	task->regs.edi = 0;
+	task->regs.eflags = flags;
+	task->regs.eip = (uint32t)code;
+	task->regs.cr3 = (uint32t)*memRepo;
+	//task->regs.esp = (uint32t) ;
+}
+
+void task_yield(void)
+{
+}
